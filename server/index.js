2c38e24e425a36efebfd739b6fc4863df3b15e8c
kmkhami <nicholas.khami@gmail.com>
2021-07-26 01:21:05 -0500

    Working express server and react app

---
+++ server/index.js
@@ -0,0 +1,19 @@
+  
+// server/index.js
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
