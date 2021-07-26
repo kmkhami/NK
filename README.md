a69e33f87a306de01e73d79ffd7f8b0fb3a9d5a2
kmkhami <nicholas.khami@gmail.com>
2021-07-26 03:17:54 -0500

    Update for commerce.js request

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # NK
 
 This is a repository designed for an online supplement store using Express.js, Node.js, React.js. and Commerce.js. 
+
+### Reccomended Software 
+* Linux or Windows WSL 
+* github account 
+* node.js installed 
--- nk-react/src/App.js
+++ nk-react/src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'; 
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = useState(null);
+  const [productList, setProductList] = useState(null); 
+  
 
-  React.useEffect(() => {
+  useEffect(() => {
+    const commerce_api_key = process.env.COMMERCE_API_KEY; 
+    const commerce_base_url = 'https://api.chec.io/v1'; 
+
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
+
+    axios.get(commerce_base_url + '/products', { 
+      headers: {
+        'X-Authorization': commerce_api_key
+      }
+    })
+      .then((productList) => setProductList(productList.message))
   }, []);
 
   return (
@@ -16,6 +29,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
+        <p>{!productList ? "Loading Products...": productList} </p>
       </header>
     </div>
   );
